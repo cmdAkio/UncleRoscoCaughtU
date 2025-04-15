5184e766230fb39839f531be3e89d600f116ae1d
Akio <g92619202@gmail.com>
2025-04-15 18:31:15 -0500

    Add files via upload

--- UncleRosco.bat
+++ UncleRosco.bat
@@ -9,6 +9,7 @@ echo heh heh heh nice to meetcha big boy
 pause
 set /a count=0
 :loop
+powershell "[console]::beep(1000, 500)" 
 start https://uncleroscohascaughtyou.neocities.org/
 set /a count+=1
 if %count% lss 5 goto loop
